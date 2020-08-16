92aee6552a5b9c9deec46eb7525aa401c02d0048
mohitpd <mohitprasad28@gmail.com>
2020-08-17 02:18:32 +0530

    notification needs to be added in gui

--- gui.py
+++ gui.py
@@ -13,46 +13,6 @@ root.config(background="#ffffff")
 logo = ImageTk.PhotoImage(Image.open("Resources/logo_icon.png"))
 logo_img = Label(root, image = logo,background="#ffffff")
 
-# Face Detection
-# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
-#
-# def face_detector(img):
-#
-# 	faces = face_cascade.detectMultiScale(img, scaleFactor=1.5, minNeighbors=5)
-# 	if faces is():
-# 		return None
-# 	else:
-# 		return faces
-#
-# def capture(id = "m"):
-#
-# 	cap = cv2.VideoCapture(0)
-# 	count=0
-# 	while(True):
-# 		ret, frame = cap.read()
-# 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-# 		faces=face_detector(gray)
-# 		if faces is not None:
-# 			for (x,y,w,h) in faces:
-# 				print(x, y, w, h)
-# 				color = (255, 0, 0)
-# 				stroke = 2
-# 				x2 = x + w
-# 				y2 = y + h
-# 				cv2.rectangle(frame, (x, y), (x2, y2), color, stroke)
-# 				face_crop = gray[y:y2, x:x2]
-# 				cv2.imwrite("face_data/"+id+"_"+str(count)+".jpg",face_crop)
-# 				count+=1
-# 				cv2.imshow('frame', frame)
-# 				if count == 50:
-# 					break
-#
-# 			if count == 50:
-# 				break
-# 		if cv2.waitKey(20) & 0xFF == ord('q'):
-# 			break
-# 	cap.release()
-# 	cv2.destroyAllWindows()
 #Function goes here
 
 def take_attendence():
@@ -86,12 +46,14 @@ def register_user():
                        foreground="#ffffff",
                        activeforeground="#ff0000"
                        )
+    notif1 = Label(register,text="",font=("Arial sans MS",14,"bold"),background="#ffffff")
     id_label.pack(pady=(30,0))
     id_input.pack()
     name_label.pack(pady=(30,0))
     name_input.pack()
     take_face.pack(pady="30")
     complete_registration.pack()
+    notif1.pack(pady=(30,0))
     register.mainloop()
 def exit_app():
     root.destroy()
---
+++ training.py
@@ -0,0 +1,31 @@
+import cv2
+from PIL import Image
+import os
+import numpy as np
+
+def getImagesData(path):
+    #path to all the images in the folder
+    imagesPath = [os.path.join(path,f) for f in os.listdir(path)]
+
+    #List to contain faces
+    faces = []
+    #List to contain Ids
+    ids = []
+
+    for imagePath in imagesPath:
+        pilImage = Image.open(imagepath).convert('L')
+        imgnp = np.array(pilImage, 'uint8')
+
+        Id = int(os.path.split(imagePath)[-1].split("_")[0])
+
+        faces.append(imgnp)
+        ids.append(Id)
+
+    return faces, ids
+
+def Training():
+    recognizer = cv2.face_LBPHFaceRecognizer.create()
+    detector = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+    faces, ids = getImagesData('face_data')
+    recognizer.train(faces, np.array(ids))
+    recognizer.save('Trained_Recognizer/trainner.yml')
